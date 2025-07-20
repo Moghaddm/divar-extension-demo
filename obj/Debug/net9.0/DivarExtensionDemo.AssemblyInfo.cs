c73a3f12378bdc2f1d14c0979ad9ddee7acedafe
mohammad-mahdi <mahdimoghaddamwork@gmail.com>
2025-07-20 11:57:08 +0330

    Add comprehensive .gitignore for Visual Studio and update build artifacts.

--- obj/Debug/net9.0/DivarExtensionDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/DivarExtensionDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivarExtensionDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b7b0e75f65349d63071a7bfb35b8a9fa1b5963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1892cad494b48094014453bb85ba73f7c6354b65")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivarExtensionDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivarExtensionDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
